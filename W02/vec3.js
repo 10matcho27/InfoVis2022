32b1f52aca47cb50e2421956cdbbfd7d0e80d031
まっちょ <ryo.1027.smile@docomo.ne.jp>
2022-04-13 15:16:40 +0900

    change vec3.js

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,7 +1,9 @@
-//constructor
-Vec3 = function(x, y, z){
-    this.x = x;
-    this.y = y;
-    this.z = z;
+class Vec3
+{
+    //Constructor
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
-
