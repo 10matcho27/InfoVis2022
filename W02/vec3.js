0ad389fe50d9547a43c24631a43c748e041c8b81
まっちょ <ryo.1027.smile@docomo.ne.jp>
2022-04-13 15:32:36 +0900

    change task2.html

--- W02/vec3.js
+++ W02/vec3.js
@@ -6,4 +6,21 @@ class Vec3
         this.y = y;
         this.z = z;
     }
+    min(){
+        Math.min(this.x, this.y, this.z);
+    }
+    max(){
+        Math.max(this.x, this.y, this.z);
+    }
+    mid(){
+        if(this.x != this.min() && this.x != this.max()){
+            return this.x;
+        }else{
+            if(this.y != this.min() && this.y != this.max()){
+                return this.y;
+            }else{
+                return this.z;
+            }
+        }
+    }
 }
