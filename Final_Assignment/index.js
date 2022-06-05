104d1517012f4aac43f48ff6411f9ce6c363230b
まっちょ <ryo.1027.smile@docomo.ne.jp>
2022-06-05 20:51:10 +0900

    add Final Task

--- Final_Assignment/index.js
+++ Final_Assignment/index.js
@@ -1,30 +1,25 @@
-d3.csv("https://10matcho27.github.io/InfoVis2022/Final_Assignment/js/mikan.csv")
-    .then(data => {
-        data.forEach(d => {
-            //label,value,i,x,y,r,c
-            // d.label = d.label;
-            d.value = +d.value;
-            // d.i = d.i
-            // d.x = +d.x;
-            // d.y = +d.y;
-            // d.r = +d.r;
-            // d.c = d.c;
-        })
-        var config = {
-            parent: '#drawing_region',
-            width: 600,
-            height: 600,
-            scale: 1500,
-            margin: { top: 20, right: 20, bottom: 20, left: 20, top_title: 30 },
-        };
+d3.csv("https://10matcho27.github.io/InfoVis2022/Final_Assignment/assets/pref_data.csv", data => {
+    // data.forEach(d => {
+    //     //label,value,i,x,y,r,c
+    //     // d.label = d.label;
+    //     d.value = +d.value;
+    //     // d.i = d.i
+    //     // d.x = +d.x;
+    //     // d.y = +d.y;
+    //     // d.r = +d.r;
+    //     // d.c = d.c;
+    // })
+    var config = {
+        parent: '#drawing_region',
+        width: 1000,
+        height: 1000,
+        scale: 1500,
+        margin: { top: 20, right: 20, bottom: 20, left: 20, top_title: 30 },
+    };
 
-        let japan_map = new Japan(config, data);
-        japan_map.update();
+    let japan_map = new Japan(config, data);
 
-    })
-    .catch(error => {
-        console.log(error);
-    });
+})
 
 class Japan {
     constructor(config, data) {
@@ -38,48 +33,44 @@ class Japan {
         this.init();
     }
 
+
     init() {
-        let self = this;
+        var width = 600,
+            height = 600;
+        var scale = 1500;
+        d3.json("https://10matcho27.github.io/InfoVis2022/Final_Assignment/assets/japan.geojson", createMap);
 
-        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
-        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+        function createMap(japan) {
+            var aProjection = d3.geoMercator()
+                .center([136.0, 35.6])
+                .translate([width / 2, height / 2])
+                .scale(scale);
+            var geoPath = d3.geoPath().projection(aProjection);
+            var svg = d3.select("svg").attr("width", width).attr("height", height);
 
-        self.projection = d3.geoMercator()
-            .center([136.0, 35.6])
-            .scale(self.config.scale)
-            .translate(self.inner_width / 2, self.inner_height / 2);
+            //マップ描画
+            var map = svg.selectAll("path").data(japan.features)
+                .enter()
+                .append("path")
+                .attr("d", geoPath)
+                .style("stroke", "#ffffff")
+                .style("stroke-width", 0.1)
+                .style("fill", "#5EAFC6")
 
-        self.geoPath = d3.geoPath(self.projection);
-
-        self.svg = d3.select(self.config.parent)
-            .append('svg')
-            .attr('width', self.config.width)
-            .attr('height', self.config.height);
-        self.chart = self.svg.append('g')
-    }
-
-    update() {
-        let self = this;
-        d3.json("https://10matcho27.github.io/InfoVis2022/Final_Assignment/assets/japan.geo.json")
-            .then(function(jpn) {
-                //マップ描画
-                self.chart.selectAll("path").data(jpn.features)
-                    .enter()
-                    .append("path")
-                    .attr("d", self.geoPath)
-                    .style("stroke", "#ffffff")
-                    .style("stroke-width", 0.1)
-                    .style("fill", "#5EAFC6");
-
-                //ズームイベント設定    
-                var zoom = d3.zoom().on('zoom', function() {
-                    self.projection.scale(scale * d3.event.transform.k);
-                    map.attr('d', self.geoPath);
-                });
-                self.chart.call(zoom);
-            })
-            .catch(error => {
-                console.log(error);
+            //ズームイベント設定    
+            var zoom = d3.zoom().on('zoom', function() {
+                aProjection.scale(scale * d3.event.transform.k);
+                map.attr('d', geoPath);
             });
+            svg.call(zoom);
+
+            //ドラッグイベント設定
+            var drag = d3.drag().on('drag', function() {
+                var tl = aProjection.translate();
+                aProjection.translate([tl[0] + d3.event.dx, tl[1] + d3.event.dy]);
+                map.attr('d', geoPath);
+            });
+            map.call(drag);
+        }
     }
 }
